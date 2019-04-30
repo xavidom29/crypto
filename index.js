af7a2be20c8d89609ff7b3584295dac8c0ca6356
xavidominguez <xavibcn1991@gmail.com>
2019-04-30 16:22:38 +0200

    first commit

--- index.js
+++ index.js
@@ -11,21 +11,19 @@ function getCryptoInfo(nombrecrypto) {
       mostrarInfo(data)
     }
   };
-  requester.open('GET', "https://api.coinlore.com/api/tickers/?id=" + nombrecrypto);
+  requester.open('GET', "https://api.coinlore.com/api/tickers/" + nombrecrypto);
   requester.send();
 }
 
 function mostrarInfo(cryptocurrency) {
   console.log(cryptocurrency);
-  document.querySelectorAll('#contenedor')[0].innerHTML = "<div>Name: " + cryptocurrency.nombrecrypto;
+  document.querySelectorAll('#contenedor')[0].innerHTML = "<div>Name: " + cryptocurrency.data[0].name;
 }
 
 // VARIABLES
 const botonAñadir = document.querySelectorAll('#boton')[0];
-let global = [];
 
 // BINDS Y EVENTOS
-
 
 botonAñadir.addEventListener('click', function() {
   let nombreCrypto = document.querySelectorAll('#input')[0].value;
