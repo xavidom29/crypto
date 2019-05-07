5463e237194ca10cc0a223935a2f89c19fb64cd0
xavidominguez <xavibcn1991@gmail.com>
2019-05-07 17:54:01 +0200

    first commit

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 //  FUNCIONES
 function getCryptoInfo(nombrecrypto) {
   const requester = new XMLHttpRequest();
-  requester.onreadystatechange = function() {
+    requester.onreadystatechange = function() {
     if (this.readyState != 4) {
       return
     }
     if (this.status == 200) {
-      var data = JSON.parse(this.responseText)[0];
+      var data = JSON.parse(this.responseText);
 
-      mostrarInfo(data)
+      mostrarInfo(data, nombrecrypto)
     }
   };
-  requester.open('GET', "https://api.coinlore.com/api/tickers/" + nombrecrypto);
+  requester.open('GET', "https://cors-anywhere.herokuapp.com/https://api.coinlore.com/api/tickers/");
+  requester.setRequestHeader("X-Requested-With", "XMLHttpRequest");
   requester.send();
 }
 
-function mostrarInfo(cryptocurrency) {
-  console.log(cryptocurrency);
-  document.querySelectorAll('#contenedor')[0].innerHTML = "<div>Name: " + cryptocurrency.data[0].name;
+function mostrarInfo(monedas, nombreMoneda) {
+  console.log(nombreMoneda);
+  console.log(monedas);
+  let monedaEscogida = {};
+
+  for (var i = 0; i < monedas.data.length; i++) {
+    if (monedas.data[i].name == nombreMoneda) {
+      console.log(monedaEscogida);
+      monedaEscogida = monedas.data[i];
+    }
+  }
+  document.querySelectorAll('#contenedor')[0].innerHTML = "<div>Name: " + monedaEscogida.name + "<div>Symbol: " + monedaEscogida.symbol + "<div>Price : " + monedaEscogida.price_usd + " $" + "<div>Rank : " + monedaEscogida.rank + "<div>Marquet Capital : " + monedaEscogida.market_cap_usd;
 }
 
 // VARIABLES
